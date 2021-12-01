f7a533131d052e937d262d0e544a3183e0ebf647
juniorjj13 <62919997+juniorjj13@users.noreply.github.com>
2021-12-01 18:05:13 +0000

    Add files via upload
    Clear function only works within replit environment for now

---
+++ main.py
@@ -0,0 +1,31 @@
+#clear function only works within replit environment for now
+from replit import clear
+from art import logo
+print(logo)
+
+bids = {}
+bidding_finished = False
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+while not bidding_finished:
+    name = input("What's your name?\n")
+    bid_price = int(input("What's your bid? $"))
+    bids[name] = bid_price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+    if should_continue == "no":
+        bidding_finished = True
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        clear()
+    
+
+
